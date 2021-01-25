9bc587eb66dccb4d85e93f8ecf72529fee845457
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-01-25 23:14:39 +0000

    Added Album model

--- products/views.py
+++ products/views.py
@@ -64,17 +64,17 @@ def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
     reviews = Review.objects.filter(product=product_id)
-    event   = get_object_or_404(Event, product=product_id)
+ 
 
 
-    print(event)
+
 
     #print(event.event_title)
 
     context = {
         'product': product,
         'reviews': reviews,
-        'event' : event,
+
     }
 
     return render(request, 'products/product_detail.html', context)
