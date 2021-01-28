0d4fa48064f92cefb27cef5e60374a323388c680
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-01-28 21:24:42 +0000

    Added advert model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Advert(models.Model):
+    link_url = models.URLField(max_length=1024, null=True, blank=True)
+    banner = models.ImageField(null=True, blank=True)
+    name = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
