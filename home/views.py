38494525241a91a03d50f89db1d63a87c3d58e8a
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2021-02-17 21:10:15 +0000

    Added product list onto homepage

--- home/views.py
+++ home/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Advert
-
+from products.models import Product
 
 def index(request):
     """ A view to return the index page """
 
     adverts = Advert.objects.all()
+    products = Product.objects.all()
 
     context = {
         'adverts': adverts,
+        'products': products,
     }
     return render(request, 'home/index.html', context)
+
+
